4c658c24af4f6fc66d75267d97f2d6ae88943d59
Tomas Salina <salinatomass53@gmail.com>
2025-08-31 04:39:50 -0300

    feat(): add manifest and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,23 @@ export const metadata: Metadata = {
       },
     ],
   },
+  other: {
+    "fc:frame": JSON.stringify({
+      version: "next",
+      imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+      button: {
+        title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+        action: {
+          type: "launch_frame",
+          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+          url: URL,
+          splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
+          splashBackgroundColor:
+            process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+        },
+      },
+    }),
+  },
 };
 
 export default function RootLayout({
